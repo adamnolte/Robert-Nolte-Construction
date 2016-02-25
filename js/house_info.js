fb9f47a14deec9514592bdb55514d4e23363d58b
ajnolte12 <ajnolte12@gmail.com>
2016-02-24 20:45:42 -0600

    House Info

---
+++ js/house_info.js
@@ -0,0 +1,108 @@
+//Fill in each one for each house. They are identified by name
+var duvall_info = {
+	pictures: 0, //Dont Touch This
+	sqft: 'Coming Soon!', //Square Feet
+	prop_type: 'Coming Soon!', //Property Type
+	bdr: 'Coming Soon!', //Bedrooms
+	bath: 'Coming Soon!', //Bathrooms
+	tot_rooms: 'Coming Soon!', //Total Rooms
+	year: 'Coming Soon!', //Year Completed
+	side: 'Coming Soon!', //Siding
+	gar: 'Coming Soon!', //Garage Square Feet
+	heat: 'Coming Soon!', //Heating
+	ac: 'Coming Soon!' //Air Conditioning
+}
+
+var gibson_info = {
+	pictures: 28,
+	sqft: 'Coming Soon!',
+	prop_type: 'Coming Soon!',
+	bdr: 'Coming Soon!',
+	bath: 'Coming Soon!',
+	tot_rooms: 'Coming Soon!',
+	year: 'Coming Soon!',
+	side: 'Coming Soon!',
+	gar: 'Coming Soon!',
+	heat: 'Coming Soon!',
+	ac: 'Coming Soon!'
+}
+
+var lichte_info = {
+	pictures: 49,
+	sqft: 'Coming Soon!',
+	prop_type: 'Coming Soon!',
+	bdr: 'Coming Soon!',
+	bath: 'Coming Soon!',
+	tot_rooms: 'Coming Soon!',
+	year: 'Coming Soon!',
+	side: 'Coming Soon!',
+	gar: 'Coming Soon!',
+	heat: 'Coming Soon!',
+	ac: 'Coming Soon!'
+}
+
+var linebach_info = {
+	pictures: 53,
+	sqft: 'Coming Soon!',
+	prop_type: 'Coming Soon!',
+	bdr: 'Coming Soon!',
+	bath: 'Coming Soon!',
+	tot_rooms: 'Coming Soon!',
+	year: 'Coming Soon!',
+	side: 'Coming Soon!',
+	gar: 'Coming Soon!',
+	heat: 'Coming Soon!',
+	ac: 'Coming Soon!'
+}
+
+var main_info = {
+	pictures: 5,
+	sqft: 'Coming Soon!',
+	prop_type: 'Coming Soon!',
+	bdr: 'Coming Soon!',
+	bath: 'Coming Soon!',
+	tot_rooms: 'Coming Soon!',
+	year: 'Coming Soon!',
+	side: 'Coming Soon!',
+	gar: 'Coming Soon!',
+	heat: 'Coming Soon!',
+	ac: 'Coming Soon!'
+}
+
+var moore_info = {
+	pictures: 1,
+	sqft: 'Coming Soon!',
+	prop_type: 'Coming Soon!',
+	bdr: 'Coming Soon!',
+	bath: 'Coming Soon!',
+	tot_rooms: 'Coming Soon!',
+	year: 'Coming Soon!',
+	side: 'Coming Soon!',
+	gar: 'Coming Soon!',
+	heat: 'Coming Soon!',
+	ac: 'Coming Soon!'
+}
+
+var riekhoff_info = {
+	pictures: 27,
+	sqft: 'Coming Soon!',
+	prop_type: 'Coming Soon!',
+	bdr: 'Coming Soon!',
+	bath: 'Coming Soon!',
+	tot_rooms: 'Coming Soon!',
+	year: 'Coming Soon!',
+	side: 'Coming Soon!',
+	gar: 'Coming Soon!',
+	heat: 'Coming Soon!',
+	ac: 'Coming Soon!'
+}
+
+var houses = {
+	duvall: duvall_info,
+	gibson: gibson_info,
+	lichte: lichte_info,
+	linebach: linebach_info,
+	main: main_info,
+	moore: moore_info,
+	riekhoff: riekhoff_info
+}
